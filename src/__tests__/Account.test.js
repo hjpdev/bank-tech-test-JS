fbbde2fd889056dc3e4fec786ec4d5ef9b94f2eb
Harry <harryjp1995@gmail.com>
2018-11-07 21:24:12 +0000

    First commit: Adds basic structure & first tests

---
+++ src/__tests__/Account.test.js
@@ -0,0 +1,17 @@
+import Account from '../Account'
+
+describe('Account', () => {
+  let acc
+  beforeEach(() => {
+    acc = new Account()
+  }) 
+  
+  it('Has a balance of 0 by default', () => {
+    expect(acc.getBalance()).toEqual(0)
+  })
+
+  it('Can be created with money deposited', () => {
+    acc2 = new Account(54)
+    expect(acc2.getBalance()).toEqual(54)
+  })
+})
